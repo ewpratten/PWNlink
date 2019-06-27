c8a9d2461b545231723b715ff6a858403c437492
Evan Pratten <ewpratten@gmail.com>
2019-06-27 11:20:30 -0400

    Lunch time

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ class ASCII:
     Reset = "\u001b[0m"
 
 # Router class
+
+
 class Router:
     ip: str
     model: str
     password: str
+
+# Create a blank router
+router = Router()
 
 # Attack types enum
 
@@ -31,13 +36,18 @@ class Attacks:
 
 
 # List of supported routers, and their attack type
-routers = [
-    ("DGL-5500", Attacks.USERLESS),
-    ("DIR-855L", Attacks.USERLESS),
-    ("DIR-835", Attacks.USERLESS),
-    ("DHP-1565", Attacks.USER),
-    ("DIR-652", Attacks.USER),
-]
+routers = {
+    "DGL-5500": Attacks.USERLESS,
+    "DIR-855L": Attacks.USERLESS,
+    "DIR-835": Attacks.USERLESS,
+    "DHP-1565": Attacks.USER,
+    "DIR-652": Attacks.USER
+}
+
+# TUI menu
+menu = {
+    1:{"title": "Display password", "function": lambda: print(router.password)}
+}
 
 ## FUNCTIONS ##
 
@@ -60,13 +70,14 @@ def isSupported(router_str: str) -> bool:
     """ Check if a router string is supported by this attack """
 
     # Return if router string is in the first element of each tuple of routers
-    return router_str in [router[0] for router in routers]
+    return router_str in routers
 
 
 def getPassword(ip: str, attack_type: int) -> str:
     """ Parses the router configuration for admin password """
 
     return ""
+
 
 def checkDNS() -> str:
     return socket.gethostbyname("dlinkrouter.local")
@@ -91,29 +102,49 @@ def main():
     """
     print(ASCII.Red + banner)
 
-    # Create a blank router
-    router = Router()
+    
 
     # Check for router dns
     print(ASCII.Reset + "   [ ] Search for router via DNS...", end="\r")
-    
+
     try:
         router.ip = checkDNS()
     except:
         router.ip = ""
-    
+
     if router.ip != "":
-        status_str = ASCII.Green + "   [*] " if router.model != "" else ASCII.Yellow + "   [ ] "
-        print(ASCII.Green + "   [*] " + ASCII.Reset + "Found router ip via DNS         ")
+        status_str = ASCII.Green + \
+            "   [*] " if router.model != "" else ASCII.Yellow + "   [ ] "
+        print(ASCII.Green + "   [*] " + ASCII.Reset +
+              "Found router ip via DNS         ")
     else:
         # Check for router IP
         router.ip = getRouterIP()
-        print(ASCII.Green + "   [*] " + ASCII.Reset + "Found router ip via gateway          ")
-    
+        print(ASCII.Green + "   [*] " + ASCII.Reset +
+              "Found router ip via gateway          ")
+
     # Check router type
     router.model = getRouterType(router.ip)
-    status_str = ASCII.Green + "   [*] " if router.model != "" else ASCII.Yellow + "   [ ] "
+    status_str = ASCII.Green + \
+        "   [*] " if router.model != "" else ASCII.Yellow + "   [ ] "
     print(status_str + ASCII.Reset + "Check router type")
+
+    # Check if oruter is PWNable
+    is_pwnable = isSupported(router.model)
+    status_str = ASCII.Green + \
+        "   [*] " if is_pwnable else ASCII.Yellow + "   [ ] "
+    print(status_str + ASCII.Reset + "Check router support")
+
+    # exit if router is not supported
+    if not is_pwnable:
+        print("\nRouter is not supported by PWNlink")
+        exit(1)
+
+    # Check for password
+    router.password = getPassword(router.ip, routers[router.model])
+
+    # Display the menu
+
 
 
 # Do the python thingy
